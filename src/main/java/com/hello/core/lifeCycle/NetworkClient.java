bc4507f4a6b6d8b387dd687ad9c5663483c278ed
FaberJoo <sjdk0805@gmail.com>
2023-01-12 22:56:56 +0900

    day 6 : @PostConstruct, @PreDstroy 어노테이션 적용

--- src/main/java/com/hello/core/lifeCycle/NetworkClient.java
+++ src/main/java/com/hello/core/lifeCycle/NetworkClient.java
@@ -1,5 +1,8 @@
 package com.hello.core.lifeCycle;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
@@ -26,12 +29,14 @@ public class NetworkClient {
     System.out.println("close: " + url);
   }
 
+  @PostConstruct
   public void init() {
     System.out.println("NetworkClient.init");
     connect();
     call("초기화 연결 메시지");
   }
 
+  @PreDestroy
   public void close() {
     System.out.println("NetworkClient.close");
     disconnect();
