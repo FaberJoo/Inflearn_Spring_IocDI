9a46a6dac007686399ed5b152099432ba7bb6d5b
FaberJoo <sjdk0805@gmail.com>
2023-01-14 18:06:55 +0900

    day 7 : Provider로 해결

--- src/test/java/com/hello/core/scope/PrototypeProviderTest.java
+++ src/test/java/com/hello/core/scope/PrototypeProviderTest.java
@@ -5,6 +5,7 @@ import javax.annotation.PreDestroy;
 
 import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -27,10 +28,10 @@ public class PrototypeProviderTest {
 
   static class ClientBean {
     @Autowired
-    private ApplicationContext ac;
+    private ObjectProvider<PrototypeBean> prototypeBeanProvider;
 
     public int logic() {
-      PrototypeBean prototypeBean = ac.getBean(PrototypeBean.class);
+      PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
       prototypeBean.addCount();
       int count = prototypeBean.getCount();
       return count;
