694947048ad80887b42f565999e0e791a683caec
FaberJoo <sjdk0805@gmail.com>
2023-01-15 14:44:02 +0900

    day 8 : Provider로 해결

--- src/main/java/com/hello/core/logdemo/LogDemoService.java
+++ src/main/java/com/hello/core/logdemo/LogDemoService.java
@@ -1,5 +1,6 @@
 package com.hello.core.logdemo;
 
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 import com.hello.core.common.Mylogger;
@@ -9,9 +10,10 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class LogDemoService {
-  private final Mylogger mylogger;
+  private final ObjectProvider<Mylogger> myloggerProvider;
 
   public void logic(String id) {
+    Mylogger mylogger = myloggerProvider.getObject();
     mylogger.log("service id = " + id);
   }
 }
--- src/main/java/com/hello/core/web/LogDemoController.java
+++ src/main/java/com/hello/core/web/LogDemoController.java
@@ -2,6 +2,7 @@ package com.hello.core.web;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,12 +16,13 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class LogDemoController {
   private final LogDemoService logDemoService;
-  private final Mylogger mylogger;
+  private final ObjectProvider<Mylogger> myloggerProvider;
 
   @RequestMapping("log-demo")
   @ResponseBody
   public String logDemo(HttpServletRequest request) {
     String requestURL = request.getRequestURL().toString();
+    Mylogger mylogger = myloggerProvider.getObject();
     mylogger.setRequestURL(requestURL);
 
     mylogger.log("controller test");
